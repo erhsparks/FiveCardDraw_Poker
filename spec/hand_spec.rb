b7de09693b348394f976216febe61949834e0acc
Elizabeth Sparks <erhsparks@gmail.com>
2016-09-22 00:54:31 -0700

    resolve error that prevented hands_spec from assigning dummy cards with

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -3,13 +3,11 @@ require 'rspec'
 
 describe Hand do
   subject(:hand) { Hand.new }
-  before(:each) do
-    @card1 = double("card1", :suit => :♠, :value => :"10")
-    @card2 = double("card2", :suit => :♠, :value => :J)
-    @card3 = double("card3", :suit => :♠, :value => :Q)
-    @card4 = double("card4", :suit => :♠, :value => :K)
-    @card5 = double("card5", :suit => :♠, :value => :A)
-  end
+  let(:card1) { double("card1", :suit => :♠, :value => :"10") }
+  let(:card2) { double("card2", :suit => :♠, :value => :J) }
+  let(:card3) { double("card3", :suit => :♠, :value => :Q) }
+  let(:card4) { double("card4", :suit => :♠, :value => :K) }
+  let(:card5) { double("card5", :suit => :♠, :value => :A) }
 
   describe "#initialize" do
     it "creates empty array in instance variable @card" do
@@ -26,35 +24,35 @@ describe Hand do
 
   describe "#add_cards" do
     it "adds a card to an empty hand" do
-      hand.add_cards(@card3)
+      hand.add_cards(card3)
       new_hand = hand.cards
 
       expect(new_hand.size).to eq(1)
-      expect(new_hand).to include(@card3)
+      expect(new_hand).to include(card3)
     end
 
     it "adds a card to a hand with cards in it" do
-      hand.add_cards(@card1)
-      hand.add_cards(@card2)
+      hand.add_cards(card1)
+      hand.add_cards(card2)
       new_hand = hand.cards
 
       expect(new_hand.size).to eq(2)
-      expect(new_hand).to include(@card1)
-      expect(new_hand).to include(@card2)
+      expect(new_hand).to include(card1)
+      expect(new_hand).to include(card2)
     end
 
     it "adds multiple cards to a hand" do
-      hand.add_cards(@card1, @card2, @card5)
+      hand.add_cards(card1, card2, card5)
       new_hand = hand.cards
 
       expect(new_hand.size).to eq(3)
-      expect(new_hand).to include(@card1)
-      expect(new_hand).to include(@card2)
-      expect(new_hand).to include(@card5)
+      expect(new_hand).to include(card1)
+      expect(new_hand).to include(card2)
+      expect(new_hand).to include(card5)
     end
 
     it "does not allow hand to be greater than five cards" do
-      hand.add_cards(@card1, @card2, @card3, @card4, @card5)
+      hand.add_cards(card1, card2, card3, card4, card5)
       card6 = double("card6", :suit => :♠, :value => :"9")
 
       expect { hand.add_cards(card6) }.to raise_error
@@ -63,7 +61,7 @@ describe Hand do
 
   describe "#discard" do
     before(:each) do
-      hand.add_cards(@card1, @card2, @card3, @card4, @card5)
+      hand.add_cards(card1, card2, card3, card4, card5)
     end
 
     it "raises an error if input card is not in hand" do
@@ -71,28 +69,28 @@ describe Hand do
     end
 
     it "removes a specific card from hand" do
-      hand.discard(@card2)
+      hand.discard(card2)
       new_hand = hand.cards
 
-      expect(new_hand).to_not include(@card2)
       expect(new_hand.size).to eq(4)
+      expect(new_hand).to_not include(card2)
     end
 
     it "removes multiple cards from hand" do
-      hand.discard(@card1, @card4)
+      hand.discard(card1, card4)
       new_hand = hand.cards
 
-      expect(new_hand).to_not include(@card1)
-      expect(new_hand).to_not include(@card4)
       expect(new_hand.size).to eq(3)
+      expect(new_hand).to_not include(card1)
+      expect(new_hand).to_not include(card4)
     end
   end
 
   describe "#winning_hand" do
     before(:each) do
-      hand.add_cards(@card1, @card2, @card3, @card4, @card5)
+      hand.add_cards(card1, card2, card3, card4, card5)
     end
-    
+
     let(:possible_values) { Hand::ALPHABET }
 
     before(:each) do
